c5d6e94f169df776dc2443587d2b6014e2f2b1d8
RizkiRahmann <riskirahman1116@gmail.com>
2025-07-09 10:36:32 +0700

    FILE BARU - RR

---
+++ src/main/java/com/reg/regis/RegistrationAbsoluteApplication.java
@@ -0,0 +1,13 @@
+package com.reg.regis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAbsoluteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAbsoluteApplication.class, args);
+	}
+
+}
